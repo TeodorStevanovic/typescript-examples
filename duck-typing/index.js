e03b53603028005af8fe41d6ba0d7c2593fa6ac1
Teodor <stevanovicteodor002@gmail.com>
2026-03-16 15:03:15 +0100

    setup: initialize optional-properties typescript project

--- duck-typing/index.js
+++ duck-typing/index.js
@@ -1,5 +1,20 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-let name = "Teodor";
-console.log(name);
+let admin = {
+    name: "Branko",
+    age: 55,
+    email: "brankoderic@gmail.com",
+    isAdmin: true
+};
+let person = {
+    name: "Teodor",
+    age: 24
+};
+function printUser(user) {
+    console.log(user.name);
+    console.log(user.age);
+}
+;
+printUser(admin);
+printUser(person);
 //# sourceMappingURL=index.js.map
--- duck-typing/index.ts
+++ duck-typing/index.ts
@@ -1,2 +1,24 @@
-let name: string = "Teodor";
-console.log(name);
+type User = {
+    name: string,
+    age: number
+};
+
+let admin = {
+    name: "Branko",
+    age: 55,
+    email: "brankoderic@gmail.com",
+    isAdmin: true
+};
+
+let person: User = {
+    name: "Teodor",
+    age: 24
+};
+
+function printUser(user: User) {
+    console.log(user.name);
+    console.log(user.age);
+};
+
+printUser(admin);
+printUser(person);
