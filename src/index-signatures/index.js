83954a43e78cd5a0c0eca6779c42ab85ad091298
Teodor <stevanovicteodor002@gmail.com>
2026-03-17 13:05:59 +0100

    refactor: restructure project into single TypeScript setup with organized src folder

--- src/index-signatures/index.js
+++ src/index-signatures/index.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const studentScores = {
-    "math": 90,
-    "english": 88,
-    "history": 100
+    math: 90,
+    english: 88,
+    history: 100
 };
 function printScores(student) {
     for (const key in student) {
