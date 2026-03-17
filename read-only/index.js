aebe87c91fd250cb3082c9d50e631bb28269dd99
Teodor <stevanovicteodor002@gmail.com>
2026-03-17 10:54:02 +0100

    feat: add readonly property example in TypeScript

--- read-only/index.js
+++ read-only/index.js
@@ -1,18 +1,12 @@
-type User = {
-    readonly id: number,
-    name: string,
-    age: number
-};
-
-const person: User = {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const person = {
     id: 1,
     name: "Teodor",
     age: 24,
 };
-
 // 'id' is a readonly property, meaning it can only be assigned during object creation.
 // Reassigning it later will result in a TypeScript error.
-
 // person.id = 2;
-
 console.log(person);
+//# sourceMappingURL=index.js.map
