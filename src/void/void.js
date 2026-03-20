fd53e82864e9bcfd08e12f2b6809f6320e9c73f6
Teodor <stevanovicteodor002@gmail.com>
2026-03-20 11:08:55 +0100

    feat: add void function example for logging messages

---
+++ src/void/void.js
@@ -0,0 +1,8 @@
+"use strict";
+// This function prints a message to the console and does not return any value.
+Object.defineProperty(exports, "__esModule", { value: true });
+function printMessage(message) {
+    console.log(message);
+}
+printMessage("Cao ja sam Teodor.");
+//# sourceMappingURL=void.js.map
---
+++ src/void/void.ts
@@ -0,0 +1,7 @@
+// This function prints a message to the console and does not return any value.
+
+function printMessage(message: string): void {
+    console.log(message);
+}
+
+printMessage("Cao ja sam Teodor.");
