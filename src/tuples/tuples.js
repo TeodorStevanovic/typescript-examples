b988aae1c167d2ca60b1e6052106c3b59487ba71
Teodor <stevanovicteodor002@gmail.com>
2026-03-20 10:42:34 +0100

    feat: add readonly tuple example with immutable array operations

--- src/tuples/tuples.js
+++ src/tuples/tuples.js
@@ -4,4 +4,8 @@ let person = ["Teodor", "Stevanovic", 2002];
 console.log(person);
 // Readonly tuple cannot be modified after initialization
 // person[0] = "Bosko"; // Error: Cannot assign to '0' because it is a read-only property
+// Readonly tuple cannot be modified directly.
+// Methods like concat can be used to create a new array without changing the original tuple.
+let updatePerson = person.concat("Frontend Developer");
+console.log(updatePerson);
 //# sourceMappingURL=tuples.js.map
--- src/tuples/tuples.ts
+++ src/tuples/tuples.ts
@@ -1,6 +1,13 @@
 let person: readonly [string, string, number] = ["Teodor", "Stevanovic", 2002];
 
-console.log(person)
+console.log(person);
 
 // Readonly tuple cannot be modified after initialization
 // person[0] = "Bosko"; // Error: Cannot assign to '0' because it is a read-only property
+
+// Readonly tuple cannot be modified directly.
+// Methods like concat can be used to create a new array without changing the original tuple.
+
+let updatePerson = person.concat("Frontend Developer");
+
+console.log(updatePerson);
