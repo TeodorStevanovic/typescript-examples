4080e23a17e32d7ebe888b125fcb8a688859185f
Teodor <stevanovicteodor002@gmail.com>
2026-03-25 14:01:07 +0100

    feat: implement findMax using apply with error handling for empty array

--- src/this/this.js
+++ src/this/this.js
@@ -21,4 +21,23 @@ const greetUserOne = greet.bind(userOne);
 const greetUserTwo = greet.bind(userTwo);
 console.log(greetUserOne());
 console.log(greetUserTwo());
+function findMax() {
+    let max;
+    if (this.numbers.length === 0) {
+        throw Error("Array is empty!");
+    }
+    else {
+        max = this.numbers[0];
+    }
+    for (let n = 1; n < this.numbers.length; n++) {
+        if (this.numbers[n] > max) {
+            max = this.numbers[n];
+        }
+    }
+    return max;
+}
+const arrNumbers = {
+    numbers: [3, 65, 100, 56]
+};
+console.log(findMax.apply(arrNumbers));
 //# sourceMappingURL=this.js.map
--- src/this/this.ts
+++ src/this/this.ts
@@ -38,3 +38,34 @@ const greetUserTwo = greet.bind(userTwo);
 
 console.log(greetUserOne())
 console.log(greetUserTwo())
+
+//******************************************************************//
+
+type Numbers = {
+    numbers: number[]
+}
+
+function findMax(this: Numbers) {
+    let max;
+
+    if (this.numbers.length === 0) {
+        throw Error("Array is empty!");
+    }
+    else {
+        max = this.numbers[0]
+    }
+    
+    for (let n = 1; n < this.numbers.length; n++) {
+        if (this.numbers[n] > max) {
+            max = this.numbers[n]
+        }
+    }
+
+    return max
+}
+
+const arrNumbers: Numbers = {
+    numbers: [3, 65, 100, 56]
+}
+
+console.log(findMax.apply(arrNumbers));
