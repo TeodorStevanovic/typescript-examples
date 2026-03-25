1cfa475e2d03fe23b31e2cab8b24f5bf5a83bf76
Teodor <stevanovicteodor002@gmail.com>
2026-03-25 11:27:38 +0100

    feat: add comments explaining array vs rest parameter functions

--- src/rest-parameters/rest-parameters.js
+++ src/rest-parameters/rest-parameters.js
@@ -1,11 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+// Standard function - accepts an array of numbers as a single argument
 function sumArray(numbers) {
     return numbers.reduce((total, n) => total + n, 0);
 }
 console.log(sumArray([1, 2, 3, 4]));
+// Rest parameters - accepts a variable number of arguments
 function sumAll(...numbers) {
     return numbers.reduce((total, n) => total + n, 0);
 }
-console.log(sumAll());
+console.log(sumAll(1, 4, 5, 6));
 //# sourceMappingURL=rest-parameters.js.map
--- src/rest-parameters/rest-parameters.ts
+++ src/rest-parameters/rest-parameters.ts
@@ -1,11 +1,13 @@
+// Standard function - accepts an array of numbers as a single argument
 function sumArray(numbers: number[]): number {
     return numbers.reduce((total, n) => total + n, 0)
 }
 
 console.log(sumArray([1, 2, 3, 4]))
 
+// Rest parameters - accepts a variable number of arguments
 function sumAll(...numbers: number[]): number {
     return numbers.reduce((total, n) => total + n, 0)
 }
 
-console.log(sumAll())
+console.log(sumAll(1, 4, 5, 6))
