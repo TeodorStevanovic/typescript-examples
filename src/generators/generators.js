dca5a81b81fd03af1fa9412da1b1bcb9bf636dc7
TeodorStevanovic <stevanovicteodor002@gmail.com>
2026-03-27 17:08:36 +0100

    feat: implement custom iterable range with Symbol.iterator

--- src/generators/generators.js
+++ src/generators/generators.js
@@ -1,30 +1,48 @@
 "use strict";
+// function* counter(num: number) {
 Object.defineProperty(exports, "__esModule", { value: true });
-function* counter(num) {
-    while (true) {
-        yield num++;
+//     while (true) {
+//         yield num++;
+//     }
+// };
+// const gen = counter(5);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// //*********************************************************//
+// function* fibonacci(a: number, b: number) {
+//     while(true) {
+//         yield a;
+//         [a, b] = [b, a + b];
+//     };
+// };
+// const genF = fibonacci(0, 1);
+// console.log(genF.next().value);
+// console.log(genF.next().value);
+// console.log(genF.next().value);
+// console.log(genF.next().value);
+// console.log(genF.next().value);
+const range = {
+    from: 1,
+    to: 3,
+    [Symbol.iterator]() {
+        let current = this.from;
+        let last = this.to;
+        return {
+            next() {
+                if (current <= last) {
+                    return { value: current++, done: false };
+                }
+                else {
+                    return { value: undefined, done: true };
+                }
+            }
+        };
     }
+};
+for (const num of range) {
+    console.log(num);
 }
-;
-const gen = counter(5);
-console.log(gen.next().value);
-console.log(gen.next().value);
-console.log(gen.next().value);
-console.log(gen.next().value);
-console.log(gen.next().value);
-//*********************************************************//
-function* fibonacci(a, b) {
-    while (true) {
-        yield a;
-        [a, b] = [b, a + b];
-    }
-    ;
-}
-;
-const genF = fibonacci(0, 1);
-console.log(genF.next().value);
-console.log(genF.next().value);
-console.log(genF.next().value);
-console.log(genF.next().value);
-console.log(genF.next().value);
 //# sourceMappingURL=generators.js.map
--- src/generators/generators.ts
+++ src/generators/generators.ts
@@ -1,32 +1,58 @@
-function* counter(num: number) {
+// function* counter(num: number) {
 
-    while (true) {
-        yield num++;
+//     while (true) {
+//         yield num++;
+//     }
+// };
+
+// const gen = counter(5);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+
+
+// //*********************************************************//
+
+
+// function* fibonacci(a: number, b: number) {
+//     while(true) {
+//         yield a;
+//         [a, b] = [b, a + b];
+//     };
+// };
+
+// const genF = fibonacci(0, 1);
+
+// console.log(genF.next().value);
+// console.log(genF.next().value);
+// console.log(genF.next().value);
+// console.log(genF.next().value);
+// console.log(genF.next().value);
+
+const range = {
+    from: 1,
+    to: 3,
+
+    [Symbol.iterator]() {
+        let current = this.from;
+        let last = this.to;
+
+        return {
+            next() {
+                if (current <= last) {
+                    return { value: current++, done: false }
+                }
+                else {
+                    return { value: undefined, done: true }
+                }
+            }
+        }
+
     }
 };
 
-const gen = counter(5);
-console.log(gen.next().value);
-console.log(gen.next().value);
-console.log(gen.next().value);
-console.log(gen.next().value);
-console.log(gen.next().value);
-
-
-//*********************************************************//
-
-
-function* fibonacci(a: number, b: number) {
-    while(true) {
-        yield a;
-        [a, b] = [b, a + b];
-    };
-};
-
-const genF = fibonacci(0, 1);
-
-console.log(genF.next().value);
-console.log(genF.next().value);
-console.log(genF.next().value);
-console.log(genF.next().value);
-console.log(genF.next().value);
+for (const num of range) {
+    console.log(num)
+}
