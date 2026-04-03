39800480c12221eb45a0eb05ef60d095b766b86d
Teodor <stevanovicteodor002@gmail.com>
2026-04-03 16:59:32 +0200

    feat(overload): add support for discount in order formatting

--- src/overload/overload.js
+++ src/overload/overload.js
@@ -4,10 +4,14 @@ const order = (parametar) => {
     if ("quantity" in parametar) {
         return `${parametar.name} | ${parametar.quantity}`;
     }
+    else if ("discount" in parametar) {
+        return `Name product: ${parametar.name} | Discount: ${parametar.discount}%`;
+    }
     else {
         return parametar.name;
     }
 };
 console.log(order({ name: "Igracka" }));
 console.log(order({ name: "Telefon", quantity: 20 }));
+console.log(order({ name: "Lopta", discount: 50 }));
 //# sourceMappingURL=overload.js.map
